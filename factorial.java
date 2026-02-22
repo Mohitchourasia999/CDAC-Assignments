0419939cb6c860eb497edf34d97dace988fea1b9
Mohitchourasia999 <mohit.chourasia.cmfeb26@gmail.com>
2026-02-22 20:53:36 +0530

    assignment4

---
+++ factorial.java
@@ -0,0 +1,19 @@
+package assignment4;
+
+import java.util.Scanner;
+
+public class factorial {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any number:");
+        int n=sc.nextInt();
+        int f=1;
+        for(int i=1;i<=n;i++)
+        {
+          f=f*i;
+        }
+        System.out.println("Factorial of"+n+"is :"+f);
+sc.close();
+    }
+    
+}
